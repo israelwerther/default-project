ff4094323c152fd6dbd65768bade3471d5500d9c
israelwerther <israelwerther48@outlook.com>
2023-03-20 15:28:41 -0300

    finished header and expandable side nav

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent {
 
   @Output() sideNavToggled = new EventEmitter<boolean>();
+
   menuStatus: boolean = false;
 
   SideNavToggle() {
